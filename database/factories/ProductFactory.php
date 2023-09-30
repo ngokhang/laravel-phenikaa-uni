ca23775a61b6ceec9d22c82bfec64c629e4a41cd
ngokhang <ngokhang2002vn@gmail.com>
2023-10-01 00:19:16 +0700

    add relationship, ide helper

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => fake()->text(20),
+            'price' => rand(100000, 10000000),
+            'category_id' => rand(1, 5)
+        ];
+    }
+}
